7df93dea707bd9acc6ae27b96914ede8147a69bb
vomenthan <thancn12@gmail.com>
2017-01-20 10:52:57 +0700

    update

---
+++ search-login.php
@@ -0,0 +1,70 @@
+<div class="box-search-login">
+    <div class="box-search">
+        <input class="input-search" type="text" placeholder="Nhập từ khoá">
+        <span class="advanced-search">Tìm kiếm nâng cao<i class="fa fa-search fa-2x" aria-hidden="true"></i></span>
+    </div>
+<!--    search advance-->
+    <div class="pupup-search-advance">
+        <table class="table-search">
+            <tr>
+                <td class="w30 text-key-search">
+                    <div class="key-text-search">Từ khoá</div>
+                </td>
+                <td colspan="3" class="calc">
+                    <div><input placeholder="Nhập từ khoá" type="text" class="input-key-search"></div>
+                </td>
+            </tr>
+            <tr class="edit-radio-choose">
+                <td class="w30">
+                    <div class="time-text-search">Thời gian</div>
+                </td>
+                <td class="w30">
+                    <input class="radio-choose" checked type="radio" id="ndtai" name="time" value="ndt">
+                    <label for="ndtai">Ngày đăng tải</label>
+                </td>
+                <td class="w30">
+                    <input class="radio-choose" type="radio" id="ndthau" name="time" value="ndt">
+                    <label for="ndthau">Ngày đóng thầu</label>
+                </td>
+                <td class="w30">
+                    <input class="radio-choose" type="radio" id="nph" name="time" value="nph">
+                    <label for="nph">Ngày phát hành HSMT</label>
+                </td>
+            </tr>
+            <tr>
+                <td class="w30">
+                </td>
+                <td colspan="3">
+                    <div class="w50">
+                        <span>từ ngày</span> <input  placeholder="Chọn ngày" class="input-date" type="text" id="datepicker-3">
+                        <i class="date-pic fa fa-calendar fa-2x" aria-hidden="true"></i>
+                    </div>
+
+                    <div class="w50">
+                        <span>đến ngày</span><input  placeholder="Chọn ngày" class="input-date" type="text" id="datepicker-4">
+                        <i class="date-pic fa fa-calendar fa-2x" aria-hidden="true"></i>
+                    </div>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="4">
+                    <div class="btn-search">Tìm kiếm &gt;</div>
+                </td>
+            </tr>
+        </table>
+    </div>
+<!--    end search advance -->
+
+    <div class="box-login">
+        <div class="user-login"><img src="assets/images/icon/login-user.png" alt=""><span class="name-user">admin_01</span></div>
+        <div class="pupup-login">
+            <div class="btn-lose"><img src="assets/images/icon/btn-close.png" alt=""></div>
+            <div class="username row-pup"><div class="text-login">Tên tài khoản</div><input class="input-login" type="text" placeholder="Tên tài khoản"></div>
+            <div class="password row-pup"><div class="text-login">Mật khẩu</div><input class="input-login" type="password" placeholder="Mật khẩu"></div>
+            <div class="btn-login row-pup">
+                <a class="link-qmk" href="#">Quên mật khẩu</a>
+                <div class="btn-dn"><a href="#">Đăng nhập</a></div>
+            </div>
+        </div>
+    </div>
+</div>
