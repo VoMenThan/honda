da369aa2c0a2cf88208ad323755fd4091378493c
vomenthan <thancn12@gmail.com>
2017-01-24 14:03:46 +0700

    done

---
+++ menu-left.php
@@ -0,0 +1,8 @@
+<div class="sidebar-menu">
+    <ul class="fixed-mn-left fixed">
+        <li class="ac"><a href="">Tất cả thông báo mời chào hàng</a></li>
+        <li class=""><a href="">Hàng có sẵn trên thị trường</a></li>
+        <li class=""><a href="">Hàng phải thiết kế</a></li>
+        <li class=""><a href="">Hỗn hợp</a></li>
+    </ul>
+</div>
